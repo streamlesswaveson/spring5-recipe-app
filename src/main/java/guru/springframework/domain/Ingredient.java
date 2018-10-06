d23ec0b22d50cf5aa2ff422a0cddf3614f18c586
streamlesswaveson <streamlesswaveson@gmail.com>
2018-10-06 05:49:52 -0700

    8-140 - one to many and many to one

---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,22 @@
+package guru.springframework.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private Integer amount;
+
+//    private UnitOfMeasure unitOfMeasure
+
+    @ManyToOne
+    private Recipe recipe;
+}
--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Setter
 @Getter
@@ -27,4 +28,8 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    // 'recipe' in mappedBy is the target property in the ingredient class
+    @OneToMany(cascade = CascadeType.ALL,  mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 }
