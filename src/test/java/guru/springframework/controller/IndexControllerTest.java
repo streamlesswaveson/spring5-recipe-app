01e75dce2455d6e0e11ced7e38ddfb9c9bd55ad0
streamlesswaveson <streamlesswaveson@gmail.com>
2018-10-10 04:51:02 -0700

    10-183 - mockito verify

--- src/test/java/guru/springframework/controller/IndexControllerTest.java
+++ src/test/java/guru/springframework/controller/IndexControllerTest.java
@@ -1,14 +1,18 @@
 package guru.springframework.controller;
 
+import guru.springframework.domain.Recipe;
 import guru.springframework.service.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
@@ -16,6 +20,7 @@ import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class IndexControllerTest {
 
@@ -48,5 +53,28 @@ public class IndexControllerTest {
 
         String result = controller.getIndexPage(model);
         verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+
+    @Test
+    public void usingArgumentCaptor() {
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeSet.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1l);
+        recipeSet.add(recipe);
+
+        ArgumentCaptor<Set<Recipe>> captor = ArgumentCaptor.forClass(Set.class);
+
+        when(recipeService.getRecipes()).thenReturn(recipeSet);
+
+        String result = controller.getIndexPage(model);
+
+        verify(model, times(1)).addAttribute(eq("recipes"), captor.capture());
+
+        Set<Recipe> recipes = captor.getValue();
+        assertThat(recipes.size(), is(2));
+
+
     }
 }
